fe5772edfbb8497485c95dce7c15a3da646451c5
Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
2026-03-27 06:49:58 +0100

    Revert "Launcher3: Double tap on home screen to turn off screen"
    This reverts commit 02968be7e4627cb58e7596676b1834b0bc6240ea.

--- src/com/android/launcher3/touch/WorkspaceTouchListener.java
+++ src/com/android/launcher3/touch/WorkspaceTouchListener.java
@@ -28,10 +28,8 @@ import static com.android.launcher3.logging.StatsLogManager.LauncherEvent.LAUNCH
 import static com.android.launcher3.logging.StatsLogManager.LauncherEvent.LAUNCHER_SPLIT_SELECTION_EXIT_INTERRUPTED;
 import static com.android.launcher3.logging.StatsLogManager.LauncherEvent.LAUNCHER_WORKSPACE_LONGPRESS;
 
-import android.content.Context;
 import android.graphics.PointF;
 import android.graphics.Rect;
-import android.os.PowerManager;
 import android.view.GestureDetector;
 import android.view.HapticFeedbackConstants;
 import android.view.InputDevice;
@@ -76,21 +74,15 @@ public class WorkspaceTouchListener extends GestureDetector.SimpleOnGestureListe
 
     private int mLongPressState = STATE_CANCELLED;
 
-    private final PowerManager mPm;
-
     private final GestureDetector mGestureDetector;
-
-    private final Context mContext;
 
     public WorkspaceTouchListener(Launcher launcher, Workspace<?> workspace) {
         mLauncher = launcher;
         mWorkspace = workspace;
-        mContext = workspace.getContext();
         // Use twice the touch slop as we are looking for long press which is more
         // likely to cause movement.
         mTouchSlop = 2 * ViewConfiguration.get(launcher).getScaledTouchSlop();
-        mPm = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
-        mGestureDetector = new GestureDetector(mContext, this);
+        mGestureDetector = new GestureDetector(workspace.getContext(), this);
     }
 
     @Override
@@ -229,11 +221,5 @@ public class WorkspaceTouchListener extends GestureDetector.SimpleOnGestureListe
                 cancelLongPress();
             }
         }
-    }
-
-    @Override
-    public boolean onDoubleTap(MotionEvent event) {
-        mPm.goToSleep(event.getEventTime());
-        return true;
     }
 }
